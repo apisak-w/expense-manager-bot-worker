6fbf82f9cc4f1f314b0612a95685519462275fe0
apisak-w <apisak.wongkhempetch@gmail.com>
2026-03-03 17:32:07 +0700

    feat: add CORS middleware to the Hono application

--- src/worker.ts
+++ src/worker.ts
@@ -1,9 +1,13 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import { Env } from "./types/env";
 import { handleTelegramWebhook } from "./handlers/telegram.handler";
 import { handleGetPermissions } from "./handlers/user.handler";
 
 const app = new Hono<{ Bindings: Env }>();
+
+// FIXME: Turn this into environment variable
+app.use("*", cors());
 
 app.get("/users/:userId/permissions", handleGetPermissions);
 
